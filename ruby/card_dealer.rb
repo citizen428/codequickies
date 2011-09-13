632d3152c21c90ba774b8cd86a76e35303464546
Michael Kohl <citizen428@gmail.com>
2011-09-13 20:36:20 +0200

    Use attr_accessor instead of directly manipulating instance vars

--- ruby/card_dealer.rb
+++ ruby/card_dealer.rb
@@ -1,33 +1,35 @@
 class CardGame
   DECK = %w[A 2 3 4 5 6 7 8 9 T J Q K].product(%w[c d h s]).map(&:join)
-  
+
+  attr_accessor :decks, :playing_deck, :dealt
+
   def initialize(decks=1)
-    @decks = decks
+    self.decks = decks
   end
-  
+
   def shuffle
-    @playing_deck = (DECK*@decks).shuffle
+    self.playing_deck = (DECK*self.decks).shuffle
   end
-  
+
   def deal(players=1, cards=5)
     shuffle
-    @dealt = Array.new(players) { Array.new }
-  
-    @dealt.map { |hand| cards.times { hand << @playing_deck.pop } }
+    self.dealt = Array.new(players) { Array.new }
+
+    self.dealt.map { |hand| cards.times { hand << self.playing_deck.pop } }
   end
-  
+
   def display
-    @dealt.each_with_index { |cards, i| puts "Player #{i+1}: #{cards.join(' | ')}" }
-    puts "Cards used: #{@dealt.flatten.size}"
-    puts "Cards remaining: #{@playing_deck.size}"
+    self.dealt.each_with_index { |cards, i| puts "Player #{i+1}: #{cards.join(' | ')}" }
+    puts "Cards used: #{self.dealt.flatten.size}"
+    puts "Cards remaining: #{self.playing_deck.size}"
   end
-  
+
   private :shuffle
 end
 
 game1 = CardGame.new
 game1.deal
-game1.display 
+game1.display
 puts
 game1.deal(4)
 game1.display
